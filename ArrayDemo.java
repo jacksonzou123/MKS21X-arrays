03aa708de72e46d3f2e40579e8fcbb4a79350ed1
Jackson Zou <jzou00@stuy.edu>
2018-09-26 21:26:19 -0400

    Created and tested countZeros1D function

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,7 +1,8 @@
 public class ArrayDemo{
   public static void main(String[] args) {
-    int[] arytest = new int[10];
+    int[][] arytest = new int[10][10];
     printArray(arytest);
+    System.out.println(countZeros1D(arytest[0]));
   }
   public static void printArray(int[] ary) {
     System.out.print("[");
@@ -12,7 +13,20 @@ public class ArrayDemo{
       }
     }
     System.out.print("]");
-
+    System.out.println(" ");
   }
-
+  public static void printArray(int[][] ary) {
+    for (int j = 0; j < ary.length; j++) {
+      printArray(ary[j]);
+    }
+  }
+  public static int countZeros1D(int[] ary) {
+    int f = 0;
+    for (int i = 0; i < ary.length; i++) {
+      if (ary[i] == 0) {
+        f++;
+      }
+    }
+    return f;
+  }
 }
